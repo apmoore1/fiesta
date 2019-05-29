3b852408d179830e72c28dd92005f38598f47731
Andrew Moore <andrew.p.moore94@gmail.com>
2019-05-29 11:05:25 +0100

    Setup created

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("AUTHORS.txt", "r") as fh:
+    authors = fh.read()
+
+
+setup(name='fiesta_nlp',
+      version='0.0.1',
+      description='Fast IdEntification of State-of-The-Art models using adaptive bandit algorithms',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/apmoore1/fiesta',
+      author=authors,
+      author_email='andrew.p.moore94@gmail.com',
+      license='Apache License 2.0',
+      install_requires=[
+          'numpy'
+      ],
+      python_requires='>=3.6.1',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.6'
+      ])
